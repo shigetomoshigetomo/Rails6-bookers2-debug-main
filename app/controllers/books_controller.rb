86b32431ce9331894a8db8a5c86651c3d65ffb8f
EC2 Default User <runorunonyanmyu@gmail.com>
2022-03-03 05:28:26 +0000

    [Finish]Rails6-bookers2-debug-main

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,12 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new=Book.new
   end
 
   def index
     @books = Book.all
+    @book=Book.new
   end
 
   def create
@@ -21,6 +23,9 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user_id!=current_user.id
+      redirect_to books_path
+    end
   end
 
   def update
@@ -32,9 +37,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
