a38ebfa6be2c6991cf699cadf58bd5e28536159b
EC2 Default User <runorunonyanmyu@gmail.com>
2022-03-07 08:41:13 +0000

    [Update]task6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,14 +1,16 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book=Book.find(params[:book_id])
-    comment=current_user.book_comments.new(book_comment_params)
-    comment.book_id=book.id
-    comment.save
+    @book=Book.find(params[:book_id])
+    @comment=current_user.book_comments.new(book_comment_params)
+    @comment.book_id=@book.id
+    @comment.save
+    render :index
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
+    render :index
   end
 
   private
