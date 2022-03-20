a831a291029475c1c033930fc812c5eb229e034d
EC2 Default User <runorunonyanmyu@gmail.com>
2022-03-20 15:30:26 +0000

    [Finish]task7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page]).per(5)
     @book = Book.new
+    @today_book=@books.created_today
+    @yesterday_book=@books.created_yesterday
+    @yesterday_compare=@today_book.count/@yesterday_book.count.to_f
+
+    @this_week=@books.created_this_week
+    @last_week=@books.created_last_week
+    @week_compare=@this_week.count/@last_week.count.to_f
   end
 
   def index
