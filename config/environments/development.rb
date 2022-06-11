f36aabbcaf9400d637c1515bff68cda59d995ae6
shigetomo <runorunonyanmyu@gmail.com>
2022-06-11 07:51:20 +0000

    db:migrate and install webpacker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,5 @@ Rails.application.configure do
   # config.hosts << "a8c9cb2b77b24968bf1f0967084f4a3f.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "b74c21d6c5d24d9ca9b406b9227c306b.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "f520c2bddb8d4e3fa8a203c02fcd9c4f.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -92,7 +92,6 @@ ActiveRecord::Schema.define(version: 2022_03_19_044520) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.text "introduction"
-    t.string "image_id"
     t.integer "owner_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
